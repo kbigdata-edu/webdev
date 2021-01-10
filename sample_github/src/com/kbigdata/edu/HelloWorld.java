68d595250edf745869a7bda8a3a4a39835ebfa5e
ehappyday <ehappyday@gmail.com>
2021-01-10 23:34:56 +0900

    post.html 추가 HelloWorld 변경

--- sample_github/src/com/kbigdata/edu/HelloWorld.java
+++ sample_github/src/com/kbigdata/edu/HelloWorld.java
@@ -1,6 +1,8 @@
 package com.kbigdata.edu;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,11 +20,33 @@ public class HelloWorld extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		System.out.println("HelloWorld ~~~~");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html>");
+		writer.println("<head");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<h1>안녕하세요!!!</h1>");
+		writer.println("</body>");
+		writer.println("</html>");
+		
 
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		System.out.println("doPost ~~~~");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html>");
+		writer.println("<head");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<h1>Post 방식 입니다. getPost 메소드가 호출되었습니다.</h1>");
+		writer.println("</body>");
+		writer.println("</html>");
 
 	}
 
